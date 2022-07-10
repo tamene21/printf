6f01e8aeace070d26250b0c6dfd344c45270c136
Tamene Gelaye <tamenes21@gmail.com>
2022-07-09 23:21:39 -0700

    header file

---
+++ main.h
@@ -0,0 +1,40 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
+
+
+
+/**
+ *struct print_format - Struct for printing various format
+ *@sp: The specifiers
+ *@f: The function associated
+ */
+
+typedef struct print_format
+
+{
+
+	char *sp;
+
+	int (*f)(va_list);
+
+} print_f;
+
+#endif /*MAIN_H*/
